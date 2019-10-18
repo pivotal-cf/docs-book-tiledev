5507c3b7989fd19b36de671cfaf3a2c7d904d6a5
Paul Spinrad <pspinrad@pivotal.io>
2019-10-18 10:34:52 -0700

    version number in docs-services /devguide redirects [#168278773]

--- redirects.rb
+++ redirects.rb
@@ -38,13 +38,13 @@ r301 '/', '/tiledev/2-7/index.html'
 
 r301 %r{/brokers/(\d+)-(\d+)/api.html(.*)}, 'https://github.com/openservicebrokerapi/servicebroker/blob/v2.13/spec.md'
 
-r301 %r{/brokers/devguide/services/index.html}, 'https://docs.cloudfoundry.org/devguide/services/index.html'
-r301 %r{/brokers/devguide/services/log-management.html}, 'https://docs.cloudfoundry.org/devguide/services/log-management.html'
-r301 %r{/brokers/devguide/services/managing-services.html}, 'https://docs.cloudfoundry.org/devguide/services/managing-services.html'
-r301 %r{/brokers/devguide/services/route-binding.html}, 'https://docs.cloudfoundry.org/devguide/services/route-binding.html'
-r301 %r{/brokers/devguide/services/service-keys.html}, 'https://docs.cloudfoundry.org/devguide/services/service-keys.html'
-r301 %r{/brokers/devguide/services/sharing-instances.html}, 'https://docs.cloudfoundry.org/devguide/services/sharing-instances.html'
-r301 %r{/brokers/devguide/deploy-apps/environment-variable.html}, 'https://docs.cloudfoundry.org/devguide/deploy-apps/environment-variable.html'
+r301 %r{/brokers/(\d+)-(\d+)/devguide/services/index.html}, 'https://docs.cloudfoundry.org/devguide/services/index.html'
+r301 %r{/brokers/(\d+)-(\d+)/devguide/services/log-management.html}, 'https://docs.cloudfoundry.org/devguide/services/log-management.html'
+r301 %r{/brokers/(\d+)-(\d+)/devguide/services/managing-services.html}, 'https://docs.cloudfoundry.org/devguide/services/managing-services.html'
+r301 %r{/brokers/(\d+)-(\d+)/devguide/services/route-binding.html}, 'https://docs.cloudfoundry.org/devguide/services/route-binding.html'
+r301 %r{/brokers/(\d+)-(\d+)/devguide/services/service-keys.html}, 'https://docs.cloudfoundry.org/devguide/services/service-keys.html'
+r301 %r{/brokers/(\d+)-(\d+)/devguide/services/sharing-instances.html}, 'https://docs.cloudfoundry.org/devguide/services/sharing-instances.html'
+r301 %r{/brokers/(\d+)-(\d+)/devguide/deploy-apps/environment-variable.html}, 'https://docs.cloudfoundry.org/devguide/deploy-apps/environment-variable.html'
 
 
 # tiledev redirect links to outdated/pre 2.3 tiledev references topics
