dbc2d7e79a5e1ad384c1d1b18cc7d03c0f625ee1
Paul Spinrad <pspinrad@pivotal.io>
2019-10-18 16:24:09 -0700

    reconfig edge branch to use docs-services like master branch [#168278773]

--- redirects.rb
+++ redirects.rb
@@ -1,4 +1,4 @@
-r301 '/', '/tiledev/2-3/index.html'
+r301 '/', '/tiledev/2-7/index.html'
 
 r301 %r{/pivotalcf/packaging/(.*)}, '/tiledev/$1'
 r301 %r{/pivotalcf/partners/(.*)}, '/tiledev/$1'
@@ -23,7 +23,20 @@ r301 '/tiledev/other-integrations.html', '/tiledev/stages.html'
 r301 '/tiledev/overview.html', '/tiledev/index.html'
 r301 '/tiledev/workflow.html', '/tiledev/index.html'
 
-r301 %r{/tiledev/(\d+\-\d+\/|)(roadmap|development|overview|workflow)(\.html)}, "/tiledev/$1index.html"
-r301 %r{/tiledev/(\d+\-\d+\/|)(other-integrations\.html)}, "/tiledev/$1stages.html"
+r301 %r{/tiledev/2-n/(roadmap|development|overview|workflow)(\.html)}, "/tiledev/$1index.html"
+r301 %r{/tiledev/2-n/(other-integrations\.html)}, "/tiledev/$1stages.html"
 
-r301 %r{/tiledev/(?![\d-]+)(.*)}, "/tiledev/2-3/$1"
+r301 %r{/tiledev/(?![\d-]+)(.*)}, "/tiledev/2-n/$1"
+r301 %r{/brokers/(?![\d-]+)(.*)}, "/brokers/2-n/$1"
+
+# For links in docs-dev-guide topics published in tiledev under /brokers, redirect to original OSS docs locations
+
+r301 %r{/brokers/2-n/api.html(.*)}, 'https://github.com/openservicebrokerapi/servicebroker/blob/v2.13/spec.md'
+
+r301 %r{/brokers/2-n/devguide/services/index.html}, 'https://docs.cloudfoundry.org/devguide/services/index.html'
+r301 %r{/brokers/2-n/devguide/services/log-management.html}, 'https://docs.cloudfoundry.org/devguide/services/log-management.html'
+r301 %r{/brokers/2-n/devguide/services/managing-services.html}, 'https://docs.cloudfoundry.org/devguide/services/managing-services.html'
+r301 %r{/brokers/2-n/devguide/services/route-binding.html}, 'https://docs.cloudfoundry.org/devguide/services/route-binding.html'
+r301 %r{/brokers/2-n/devguide/services/service-keys.html}, 'https://docs.cloudfoundry.org/devguide/services/service-keys.html'
+r301 %r{/brokers/2-n/devguide/services/sharing-instances.html}, 'https://docs.cloudfoundry.org/devguide/services/sharing-instances.html'
+r301 %r{/brokers/2-n/devguide/deploy-apps/environment-variable.html}, 'https://docs.cloudfoundry.org/devguide/deploy-apps/environment-variable.html'
